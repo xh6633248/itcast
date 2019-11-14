e29f4cc65e3066cc6c20440e525aba2145f1e054
Administrator <111@163.com>
2019-11-14 15:28:47 +0800

    this is verifed flie

--- src/main/java/com/itheima/git/User.java
+++ src/main/java/com/itheima/git/User.java
@@ -2,6 +2,6 @@ package com.itheima.git;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("你好,git");
+        System.out.println("this is cloned class");
     }
 }
